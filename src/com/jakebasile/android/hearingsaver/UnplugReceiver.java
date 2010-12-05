e838fa44d72a72ed6f253f58a8c80a6f34e40bdf
jakethecoder <devnull@localhost>
2010-12-04 20:47:56 -0500

    Minor code formatting and added a description even though I don't think that is displayed anywhere. Added licensing blurb to all source files.

--- src/com/jakebasile/android/hearingsaver/UnplugReceiver.java
+++ src/com/jakebasile/android/hearingsaver/UnplugReceiver.java
@@ -1,3 +1,18 @@
+/* 
+ * Copyright 2010 Jake Basile
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jakebasile.android.hearingsaver;
 
 import android.content.BroadcastReceiver;
@@ -8,6 +23,11 @@ import android.media.AudioManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+/**
+ * Handles the Headset Plug broadcast action. Cannot be registered in the manifest but must be
+ * manually registered by a service.
+ * @author Jake Basile
+ */
 public class UnplugReceiver extends BroadcastReceiver
 {
 	@Override
