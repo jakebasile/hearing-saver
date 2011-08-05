9e07717834d6692a26f76e5954aa19f5a770022b
Jake Basile <jakebasile@me.com>
2011-08-05 19:49:11 -0400

    closes issue #5 - adds backup capability. Bumped app to Android 2.2+.

--- src/com/jakebasile/android/hearingsaver/SetupActivity.java
+++ src/com/jakebasile/android/hearingsaver/SetupActivity.java
@@ -1,12 +1,9 @@
-/* 
+/*
  * Copyright 2010-2011 Jake Basile
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +14,11 @@ package com.jakebasile.android.hearingsaver;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.backup.BackupManager;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -57,6 +55,7 @@ public final class SetupActivity extends Activity
 				settings.setPluggedLevel(pluggedBar.getProgress() / 100f);
 				settings.setUnpluggedLevel(unpluggedBar.getProgress() / 100f);
 				settings.setMuteOnPlug(muteBox.isChecked());
+				BackupManager.dataChanged("com.jakebasile.android.hearingsaver");
 				startService(serviceIntent);
 				finish();
 			}
