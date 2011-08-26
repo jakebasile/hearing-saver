4dc0e265aa20528e955b56cda42ebb141d43470b
Jake Basile <jakebasile@me.com>
2011-08-26 17:52:59 -0400

    closes issue #6 - Bluetooth audio/video devices being connected or disconnected now trigger Hearing Saver.

--- src/com/jakebasile/android/hearingsaver/RegistrationService.java
+++ src/com/jakebasile/android/hearingsaver/RegistrationService.java
@@ -1,12 +1,9 @@
-/* 
+/*
  * Copyright 2010-2011 Jake Basile
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +13,7 @@
 package com.jakebasile.android.hearingsaver;
 
 import android.app.Service;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
@@ -44,6 +42,10 @@ public class RegistrationService extends Service
 		// set up the receiver.
 		registerReceiver(UnplugReceiver.getInstance(), new IntentFilter(
 			Intent.ACTION_HEADSET_PLUG));
+		registerReceiver(UnplugReceiver.getInstance(), new IntentFilter(
+			BluetoothDevice.ACTION_ACL_CONNECTED));
+		registerReceiver(UnplugReceiver.getInstance(), new IntentFilter(
+			BluetoothDevice.ACTION_ACL_DISCONNECTED));
 	}
 
 	@Override
--- src/com/jakebasile/android/hearingsaver/UnplugReceiver.java
+++ src/com/jakebasile/android/hearingsaver/UnplugReceiver.java
@@ -1,12 +1,9 @@
-/* 
+/*
  * Copyright 2010-2011 Jake Basile
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +12,16 @@
  */
 package com.jakebasile.android.hearingsaver;
 
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 
 /**
- * Handles the Headset Plug broadcast action. Cannot be registered in the manifest but must be
+ * Handles the Headset Plug broadcast action. Cannot be registered in the manifest but
+ * must be
  * manually registered by a service.
  * @author Jake Basile
  */
@@ -55,22 +55,52 @@ public class UnplugReceiver extends BroadcastReceiver
 		ignoreNext = true;
 	}
 
+	private int processIntent(Context context, Intent intent)
+	{
+		int state = -1;
+		if(intent.getAction().equals(Intent.ACTION_HEADSET_PLUG))
+		{
+			state = intent.getIntExtra("state", -1);
+		}
+		else
+		{
+			// it's a bluetooth broadcast.
+			BluetoothDevice device = (BluetoothDevice)intent.getExtras().get(
+				BluetoothDevice.EXTRA_DEVICE);
+			BluetoothClass btClass = device.getBluetoothClass();
+			int classId = btClass.getMajorDeviceClass();
+			if(classId == BluetoothClass.Device.Major.AUDIO_VIDEO)
+			{
+				if(intent.getAction().equals(BluetoothDevice.ACTION_ACL_CONNECTED))
+				{
+					state = 1;
+				}
+				else if(intent.getAction().equals(BluetoothDevice.ACTION_ACL_DISCONNECTED))
+				{
+					state = 0;
+				}
+			}
+		}
+		return state;
+	}
+
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
 		if(!ignoreNext)
 		{
-			int state = intent.getIntExtra("state", -1);
 			AudioManager am = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
 			int maxVol = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 			VolumeSettings settings = new VolumeSettings(context);
+			int state = processIntent(context, intent);
 			switch(state)
 			{
 				// unplugged.
 				case 0:
 				{
 					int newVol = (int)(maxVol * settings.getUnpluggedLevel());
-					am.setStreamVolume(AudioManager.STREAM_MUSIC, newVol, AudioManager.FLAG_SHOW_UI);
+					am.setStreamVolume(AudioManager.STREAM_MUSIC, newVol,
+						AudioManager.FLAG_SHOW_UI);
 					if(settings.getMuteOnPlug())
 					{
 						am.setRingerMode(settings.getRinger());
@@ -81,7 +111,8 @@ public class UnplugReceiver extends BroadcastReceiver
 				case 1:
 				{
 					int newVol = (int)(maxVol * settings.getPluggedLevel());
-					am.setStreamVolume(AudioManager.STREAM_MUSIC, newVol, AudioManager.FLAG_SHOW_UI);
+					am.setStreamVolume(AudioManager.STREAM_MUSIC, newVol,
+						AudioManager.FLAG_SHOW_UI);
 					if(settings.getMuteOnPlug())
 					{
 						settings.setRinger(am.getRingerMode());
